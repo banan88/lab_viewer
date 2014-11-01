d3c1c38d2a78654a4a2abaefa753f48f41293b0b
Mateusz Banaszkiewicz <mat.banaszkiewicz@gmail.com>
2014-11-01 13:27:30 +0100

    lab details first version partial working

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,6 @@ angular.module('LabViewerApp').config(['$routeProvider', routeHandler]);
 
 function routeHandler($routeProvider) {
     $routeProvider
-        .when("/", {templateUrl: "partials/all_labs.html", controller: "LabController"});
+        .when("/", {templateUrl: "partials/all_labs.html", controller: "LabController"})
+        .when("/lab/:param", {templateUrl: "partials/lab_details.html", controller: "LabController"});
 }
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,9 +4,17 @@ angular.module('Controllers', ['Services']);
 /*controller declaration*/
 angular.module('Controllers').controller('LabController', labControllerHandler);
 
-function labControllerHandler($scope, Lab) {
-    $scope.labs = Lab.get();
+function labControllerHandler($scope, $routeParams, Lab) {
+    var labId = $routeParams.param;
+    if (labId) {
+        Lab.get({id: labId}).$promise.then(function (result) {
+            $scope.lab = result;
+            $scope.parentNode = Lab.get({id: $scope.lab.parent_id});
+        });
+    } else {
+        $scope.labs = Lab.get();
+    }
 };
 /*injecting from Services module*/
-labControllerHandler.$inject = ['$scope','Lab']
+labControllerHandler.$inject = ['$scope', '$routeParams', 'Lab']
 
