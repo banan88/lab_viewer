dc0c56dace5488ed6c896f1f14d73656bd330059
Mateusz Banaszkiewicz <mat.banaszkiewicz@gmail.com>
2014-11-13 20:50:45 +0100

    refactored templates, fixed breadcrumbs links using rootScope

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,18 +4,22 @@ angular.module('Controllers', ['Services']);
 angular.module('Controllers').controller('LabController', labControllerHandler);
 angular.module('Controllers').controller('TagController', tagControllerHandler);
 
-function labControllerHandler($scope, $routeParams, Lab) {
+function labControllerHandler($scope, $rootScope, $routeParams, Lab) { //TODO refactor this
     var labId = $routeParams.param;
     if (labId) {
         Lab.get({id: labId}).$promise.then(function (result) {
             $scope.lab = result;
             $scope.parentNode = Lab.get({id: $scope.lab.parent_id});
+            $rootScope.resourceId = labId;
+            $rootScope.resourceName = $scope.lab.name;
         });
     } else {
         $scope.labs = Lab.get();
+        $rootScope.resourceId = undefined;
+        $rootScope.resourceName = undefined;
     }
 };
-labControllerHandler.$inject = ['$scope', '$routeParams', 'Lab']
+labControllerHandler.$inject = ['$scope', '$rootScope', '$routeParams', 'Lab']
 
 function tagControllerHandler($scope, $routeParams, Tag) {
     var tagId = $routeParams.param;
@@ -26,4 +30,3 @@ function tagControllerHandler($scope, $routeParams, Tag) {
     }
 };
 tagControllerHandler.$inject = ['$scope', '$routeParams', 'Tag']
-
