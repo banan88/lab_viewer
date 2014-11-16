2f7ba170c109cb9b3ea4b65b44718b469665dc1d
Mateusz Banaszkiewicz <mat.banaszkiewicz@gmail.com>
2014-11-16 16:47:33 +0100

    wrapped app in tornado http server for production purposes

--- lab_viewer.py
+++ lab_viewer.py
@@ -1,11 +1,16 @@
 import os
 
 from flask import Flask, make_response
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from app.services.api import create_api_manager
 from app.shared import db
 
+
 database_driver = 'sqlite:///'
 database_path = 'db/lab_viewer.db'
+is_debug_mode = True
 
 
 def create_app():
@@ -13,11 +18,12 @@ def create_app():
     Creates & configures flask application object and associates sqlalchemy db object with it.
     """
     app = Flask(__name__, static_url_path='')
-    app.debug = True
+    app.debug = is_debug_mode
     app.config['SQLALCHEMY_DATABASE_URI'] = database_driver + database_path
     db.app = app
     db.init_app(app)
     return app
+
 
 def create_db_schema():
     """
@@ -32,10 +38,17 @@ def create_db_schema():
 app = create_app()
 create_api_manager(app)
 
+
 @app.route('/')
 def home():
     return make_response(open('static/home.html').read())
 
+
 if __name__ == '__main__':
     create_db_schema()
-    app.run()
+    if app.debug:
+        app.run()
+    else:
+        http_server = HTTPServer(WSGIContainer(app))
+        http_server.listen(5000)
+        IOLoop.instance().start()
